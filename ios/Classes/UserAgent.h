9df059351e80a5163608f1c8a1354550874ef01f
KROTH, Caciano <caciano.kroths@gmail.com>
2021-10-15 15:52:17 -0300

    chore(*): Update version and dependencies

--- ios/Classes/UserAgent.h
+++ ios/Classes/UserAgent.h
@@ -1,4 +1,4 @@
 // Generated file, do not edit
-#define LIBRARY_VERSION @"0.0.17"
+#define LIBRARY_VERSION @"0.0.18"
 #define LIBRARY_NAME @"ondoc_flut_opentok"
-#define OPENTOK_LIBRARY_VERSION @"2.20.0"
+#define OPENTOK_LIBRARY_VERSION @"2.21.0"
